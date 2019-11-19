7ddb6f2dcec09e95a74a623863d0dddb73d7d695
Maria Ulloa <maria.ulloa@testgroup.cl>
2019-11-18 22:12:18 -0300

    diseño 18-11

--- src/app/pages/content/inicio/consulta/consulta.component.ts
+++ src/app/pages/content/inicio/consulta/consulta.component.ts
@@ -9,120 +9,128 @@ export class ConsultaComponent implements OnInit {
   name: string;
 
   energySources: any[] = [
-    { value: 'hydro', name: 'Hydro-electric' },
-    { value: 'oil', name: 'Oil' },
-    { value: 'gas', name: 'Natural gas' },
-    { value: 'coal', name: 'Coal' },
-    { value: 'nuclear', name: 'Nuclear' }
+    { value: 'VIP', name: 'VIP' },
+    { value: 'Platea', name: 'Platea' },
+    { value: 'General', name: 'General' }
   ];
 
   countriesInfo  = [{
-  country: 'USA',
-  hydro: 59.8,
-  oil: 937.6,
-  gas: 582,
-  coal: 564.3,
-  nuclear: 187.9
-}, {
-  country: 'China',
-  hydro: 74.2,
-  oil: 308.6,
-  gas: 35.1,
-  coal: 956.9,
-  nuclear: 11.3
-}, {
-  country: 'Russia',
-  hydro: 40,
-  oil: 128.5,
-  gas: 361.8,
-  coal: 105,
-  nuclear: 32.4
-}, {
-  country: 'Japan',
-  hydro: 22.6,
-  oil: 241.5,
-  gas: 64.9,
-  coal: 120.8,
-  nuclear: 64.8
-}, {
-  country: 'India',
-  hydro: 19,
-  oil: 119.3,
-  gas: 28.9,
-  coal: 204.8,
-  nuclear: 3.8
-}, {
-  country: 'Germany',
-  hydro: 6.1,
-  oil: 123.6,
-  gas: 77.3,
-  coal: 85.7,
-  nuclear: 37.8
-}];
+    country: '13-11-2019',
+    VIP: 59.8,
+    Platea: 937.6,
+    General: 582
+  }, {
+    country: '14-11-2019',
+    VIP: 74.2,
+    Platea: 308.6,
+    General: 35.1
+  }, {
+    country: '15-11-2019',
+    VIP: 40,
+    Platea: 128.5,
+    General: 361.8
+  }, {
+    country: '16-11-2019',
+    VIP: 22.6,
+    Plantea: 241.5,
+    General: 64.9
+  }, {
+    country: '17-11-2019',
+    VIP: 19,
+    Platea: 119.3,
+    General: 28.9
+  }, {
+    country: '18-11-2019',
+    VIP: 6.1,
+    Platea: 123.6,
+    General: 77.3
+  }];
+
+
+
+  energySources2: any[] = [
+    { value: 'p1', name: 'p1' },
+    { value: 'p2', name: 'p2' },
+    { value: 'p3', name: 'p3' },
+    { value: 'Estacionamiento', name: 'Estacionamiento' },
+    { value: 'Guardarropa', name: 'Guardarropa' }
+  ];
+
+  countriesInfo2  = [{
+    country: '13-11-2019',
+    Estacionamiento: 564.3,
+    Guardarropa: 187.9
+  }, {
+    country: '14-11-2019',
+    Estacionamiento: 956.9,
+    Guardarropa: 11.3
+  }, {
+    country: '15-11-2019',
+    Estacionamiento: 105,
+    Guardarropa: 32.4
+  }, {
+    country: '16-11-2019',
+    Estacionamiento: 120.8,
+    Guardarropa: 64.8
+  }, {
+    country: '17-11-2019',
+    Estacionamiento: 204.8,
+    Guardarropa: 3.8
+  }, {
+    country: '18-11-2019',
+    Estacionamiento: 85.7,
+    Guardarropa: 37.8
+  }];
 
   employees = [{
     ID: 1,
-    Nombre: 'John',
-    Apellido: 'Heart',
-    Rut: '1-9',
-    Correo: 'admin@prueba.cl',
+    NombreActividad: 'John',
+    FechaInicio: 'Heart',
+    FechaTermino: '1-9',
+    Trabajador: 'admin@prueba.cl',
     Estado: '1',
-    Contraseña: '1234',
-    Perfil: '3',
   }, {
     ID: 2,
-    Nombre: 'Robert',
-    Apellido: 'Reagan',
-    Rut: '1-9',
-    Correo: 'trabajador@prueba.cl',
+    NombreActividad: 'Robert',
+    FechaInicio: 'Reagan',
+    FechaTermino: '1-9',
+    Trabajador: 'trabajador@prueba.cl',
     Estado: '2',
-    Contraseña: '1234',
-    Perfil: '1',
   }, {
     ID: 3,
-    Nombre: 'Greta',
-    Apellido: 'Sis',
-    Rut: '1-9',
-    Correo: 'trabajador@prueba.cl',
+    NombreActividad: 'Greta',
+    FechaInicio: 'Sis',
+    FechaTermino: '1-9',
+    Trabajador: 'trabajador@prueba.cl',
     Estado: '1',
-    Contraseña: '1234',
-    Perfil: '1',
   }, {
     ID: 4,
-    Nombre: 'Brett',
-    Apellido: 'Wade',
-    Rut: '1-9',
-    Correo: 'admin@prueba.cl',
+    NombreActividad: 'Brett',
+    FechaInicio: 'Wade',
+    FechaTermino: '1-9',
+    Trabajador: 'admin@prueba.cl',
     Estado: '1',
-    Contraseña: '1234',
-    Perfil: '3',
   }, {
     ID: 5,
-    Nombre: 'Sandra',
-    Apellido: 'Johnson',
-    Rut: '1-9',
-    Correo: 'organizador@prueba.cl',
+    NombreActividad: 'Sandra',
+    FechaInicio: 'Johnson',
+    FechaTermino: '1-9',
+    Trabajador: 'organizador@prueba.cl',
     Estado: '1',
-    Contraseña: '1234',
-    Perfil: '2',
   }, {
     ID: 6,
-    Nombre: 'Kevin',
-    Apellido: 'Carter',
-    Rut: '1-9',
-    Correo: 'trabajador@prueba.cl',
+    NombreActividad: 'Kevin',
+    FechaInicio: 'Carter',
+    FechaTermino: '1-9',
+    Trabajador: 'trabajador@prueba.cl',
     Estado: '2',
-    Contraseña: '1234',
-    Perfil: '3',
   }, {
     ID: 7,
-    Nombre: 'Cynthia',
-    Apellido: 'Stanwick',
-    Rut: '1-9',
-    Correo: 'trabajador@prueba.cl',
+    NombreActividad: 'Cynthia',
+    FechaInicio: 'Stanwick',
+    FechaTermino: '1-9',
+    Trabajador: 'trabajador@prueba.cl',
     Estado: '1',
-    Contraseña: '1234',
-    Perfil: '3',
   }, {
     ID: 8,
     NombreActividad: 'Kent',
@@ -130,17 +138,13 @@ export class ConsultaComponent implements OnInit {
     FechaTermino: '1-9',
     Trabajador: 'Kent',
     Estado: '1',
-    Contraseña: '1234',
-    Perfil: '3',
   }, {
     ID: 9,
-    Nombre: 'Taylor',
-    Apellido: 'Riley',
-    Rut: '1-9',
-    Correo: 'Taylor',
-    Estado: '1',
-    Contraseña: '1234',
-    Perfil: '3',
+    NombreActividad: 'Taylor',
+    FechaInicio: 'Riley',
+    FechaTermino: '1-9',
+    Trabajador: 'Taylor',
+    Estado: '1'
   }];
 
   Estado = [{
